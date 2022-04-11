189815d6ab2c8e1b8689348aa3cf8c85b200df42
IMARGHIO <ioan.marghioala@capgemini.com>
2022-04-11 11:02:16 +0300

    Commit for Heroku

--- src/main/java/com/example/capgemini/helloheroku/HelloherokuApplication.java
+++ src/main/java/com/example/capgemini/helloheroku/HelloherokuApplication.java
@@ -2,22 +2,12 @@ package com.example.capgemini.helloheroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
-@RequestMapping("/")
 public class HelloherokuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloherokuApplication.class, args);
 	}
 
-	@RequestMapping("/hi")
-	@ResponseBody
-	String helloWorld() {
-		return "Hello World!";
-	}
 }
